e902f04d815839d670f9ec456838e9be397a19ff
sabri <sabri_51@hotmail.com>
2021-06-17 23:20:21 +0300

    vue component updated

---
+++ sentiment-analysis/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Home/>
+</template>
+<script lang="ts">
+
+import { Component, Vue } from 'vue-property-decorator'
+import Home from './components/Home'
+@Component({
+  components: {
+    Home
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
